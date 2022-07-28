18a9a8b269d4b8192f7d2b55007436d3471272d7
cophi <pzarindast@gmail.com>
2022-07-29 00:44:23 +0200

    eslint

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
